55e520e2d5fc7a2e6b0d2a9e6468e8a3addf5a9d
jjgillian <brandcraze@hotmail.com>
2019-09-12 20:39:30 -0400

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+/**
+ *
+ * @author jjgillian
+ */
+class UserService {
+
+    static UserDto createUser(UserDto userDto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
